b56f1dd00ed0a30111577eb78913f86e42488706
chibuzordaniel <starchrischibuzor@gmail.com>
2023-01-25 11:13:58 +0100

    update file

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react"
-// import  img1 from '../assests/levi-meir-clancy-h2UcC6lXlJs-unsplash 2.png'
-// import  img4 from '../assests/andres-herrera-DJPLYpriawI-unsplash 1.png'
-// import  img5 from '../assests/auguste-a-cGdVCfWpNZU-unsplash 1.png'
-// import  img6 from '../assests/mika-y-RjWd6Ol7A-unsplash 1.png'
-// import  img7 from '../assests/olenka-varzar-ot5bYzjWksE-unsplash 1.png'
 import  img8 from '../assests/Group 7.png'
 import {HiOutlineArrowNarrowRight} from 'react-icons/hi'
+import {AiOutlineSearch} from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 
 
@@ -42,21 +38,11 @@ const Art = () => {
                       <div className=" flex w-[467px] h-[66px] bg-[#1A1405] rounded-[51px]  ">
                          <form className=' flex' action="">
                               <label htmlFor=" search">
-                                    <span className=" mt-5  ml-5 pl-5">
-                                        <svg className=' -mt-1 ml-7' width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g clip-path="url(#clip0_15_4)">
-                                        <path d="M12.5 21.25C17.3325 21.25 21.25 17.3325 21.25 12.5C21.25 7.66751 17.3325 3.75 12.5 3.75C7.66751 3.75 3.75 7.66751 3.75 12.5C3.75 17.3325 7.66751 21.25 12.5 21.25Z" stroke="#FBAF00" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                        <path d="M26.25 26.25L18.75 18.75" stroke="#FBAF00" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                        </g>
-                                        <defs>
-                                        <clipPath id="clip0_15_4">
-                                        <rect width="30" height="30" fill="white"/>
-                                        </clipPath>
-                                        </defs>
-                                        </svg>
+                                    <span className="   pl-5">
+                                     < AiOutlineSearch className=" text-[#FBAF00] ml-[22px] w-[30px] h-[30px] -mt-0.5"/>
                                     </span>
                                 </label>
-                                <input   className=" w-[400px] h-[46px] pl-4 pt-[6px] font-Sora mt-2 outline-none border-none  bg-[#1A1405]" type="text" name="" id=" search" placeholder="Search" />
+                                <input   className=" w-[400px] h-[46px] pl-4 pt-[6px] font-Sora mt-2  outline-none border-none  bg-[#1A1405]" type="text" name="" id=" search" placeholder= "Search" />
                           </form>
                       </div>
                     </div>
@@ -68,14 +54,12 @@ const Art = () => {
                         {users.length > 0 && (
                             <ul className=" mt-20  grid grid-cols-3 justify-between  text-white gap-7">
                             {users.map(user => (
-                                <Link to='Home'>
                                 <li 
-                                    className="card2  font-Sora text-center  " key={user.id}> 
-                                    <img className=" card   "  src={`https://www.artic.edu/iiif/2/${user?.image_id}/full/330,/0/default.jpg`}  />
-                                    <div className="text-[24px] pt-10">{user.title} </div> 
-                                    <div className=" pt-8 ">{user.updated_at}</div>
+                                    className="card2  font-Sora   " key={user.id}> 
+                                    <Link to='Home'><img className="card"  src={`https://www.artic.edu/iiif/2/${user?.image_id}/full/330,/0/default.jpg`} alt=" art" /></Link>
+                                    <div className="text-[24px] px-5  pt-10">{user.title} </div> 
+                                    <div className=" pt-8  px-5 ">{user.updated_at}</div>
                                  </li>
-                                 </Link>
                             ))}
                             </ul>
                         )}
