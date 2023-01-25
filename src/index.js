a3238431aff6de4c34899036b5199e04627588d6
chibuzordaniel <starchrischibuzor@gmail.com>
2023-01-25 11:18:06 +0100

    update file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import  Home from './pages/Home'
-import Aristonav from './components/Aristonav'
+
 import reportWebVitals from './reportWebVitals';
 import {
   createBrowserRouter,
